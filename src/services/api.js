34953c1515c1927afcf7d78a83886fb8e6698fbf
rafamoliv <rafaoliv.ti@gmail.com>
2021-08-10 15:17:54 -0300

    kitsu api

---
+++ src/services/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+// Kitsu API, docs: https://kitsu.docs.apiary.io/#introduction/json:api
+const api = axios.create({
+  baseURL: "https://kitsu.io/api/edge/anime",
+
+  headers: {
+    Accept: "application/vnd.api+json",
+    "Content-Type": "application/vnd.api+json",
+  },
+});
+
+export default api;
