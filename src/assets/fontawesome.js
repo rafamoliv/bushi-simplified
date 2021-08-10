53742801df9a594edaa4036236ce2f87d6c88ddf
rafamoliv <rafaoliv.ti@gmail.com>
2021-08-10 00:08:39 -0300

    pre config table

--- src/assets/fontawesome.js
+++ src/assets/fontawesome.js
@@ -2,7 +2,7 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 
 // import icons
-import { faArrowDown, faFire, faInfinity, faDragon } from "@fortawesome/free-solid-svg-icons";
+import { faArrowDown, faFire, faInfinity, faDragon, faCaretRight } from "@fortawesome/free-solid-svg-icons";
 
 // add icon library
-library.add(faArrowDown, faFire, faInfinity, faDragon);
+library.add(faArrowDown, faFire, faInfinity, faDragon, faCaretRight);
--- src/components/table/table.js
+++ src/components/table/table.js
@@ -1,9 +1,43 @@
 import React from "react";
 
+// import style and assets
+import "./style.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import imageExample from "~/assets/images/imageExample.png";
+
 function Table() {
   return (
-    <div>
-      <h1>table papito</h1>
+    <div className="tableContent">
+      <div className="tableTitle">
+        <FontAwesomeIcon icon="infinity" style={{ color: "FE9800" }} size="2x" />
+        <p>Veja Animes</p>
+      </div>
+
+      <div className="tableCard">
+        <div className="animeBoard">
+          <div className="animeGrid">
+            <img className="imageCard" alt="imagem do anime" src={imageExample} />
+            <p className="animeName">Nome: Anime teste weeeee</p>
+          </div>
+
+          <div className="animeGrid">
+            <div className="animeDescriptions">
+              <span className="animeDetails">Episódios: 12</span>
+              <span className="animeDetails">Estrelas: **</span>
+            </div>
+          </div>
+
+          <div className="animeGrid">
+            <FontAwesomeIcon icon="caret-right" style={{ color: "494949" }} size="2x" />
+          </div>
+        </div>
+      </div>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
