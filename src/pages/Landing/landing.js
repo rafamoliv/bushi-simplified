07efc63c53f68e702b04f92ee4d1279aea6eed9f
rafamoliv <rafaoliv.ti@gmail.com>
2021-08-09 23:09:16 -0300

    styled changes

--- src/pages/Landing/landing.js
+++ src/pages/Landing/landing.js
@@ -2,13 +2,20 @@ import React from "react";
 import Carousel from "~/components/carousel";
 import Group from "~/components/group";
 import Navbar from "~/components/navbar";
+import Table from "~/components/table";
+
+import "./style.scss";
 
 function Landing() {
   return (
     <div className="containerLanding">
       <Navbar />
       <Carousel />
-      <Group />
+
+      <div className="bodyLanding">
+        <Group />
+        <Table />
+      </div>
     </div>
   );
 }
