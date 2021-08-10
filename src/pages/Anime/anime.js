a8848facc498645e027ce52281096d641bf852e3
rafamoliv <rafaoliv.ti@gmail.com>
2021-08-10 12:17:50 -0300

    anime

--- src/pages/Anime/anime.js
+++ src/pages/Anime/anime.js
@@ -1,9 +1,34 @@
 import React from "react";
+import Footer from "~/components/footer";
+import Navbar from "~/components/navbar";
+import narutin from "~/assets/images/narutin.png";
+import "./style.scss";
 
 function Anime() {
   return (
-    <div>
-      <h1>wee</h1>
+    <div className="animeContent">
+      <Navbar />
+      <div className="animeSectionDetails">
+        <div className="animeHeader">
+          <div className="animeImageDiv">
+            <img src={narutin} />
+          </div>
+
+          <div className="animeInformation">
+            <p>Nome do anime: Naruto</p>
+            <p>Sinopse:</p>
+            <span>
+              <br />
+              oioa sh f kdjshfak jdhfa kjshdfkajsdhfkajdaj dkajshddakljdflkadjflkasdjflkasdjflka dfkjasdlfkjasl doasjdalkjdlakdjflkajsdflkajsdf
+              dlkjalkdjfaldjfalskd daskjdalksjlkjkdasçdlaksldasjdlakjldas dlkjdalskdjalsjdkalskj
+            </span>
+          </div>
+        </div>
+        <div className="animeOtherInformation">
+          <p>Outras informações da API</p>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
