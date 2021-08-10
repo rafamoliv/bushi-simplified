cb1f653f0c2ddf20350e6c3592dd1c186728f1e9
rafamoliv <rafaoliv.ti@gmail.com>
2021-08-10 11:38:41 -0300

    creating anime route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,18 @@
 import React from "react";
+
+// react-router-dom
 import { BrowserRouter, Route } from "react-router-dom";
 
+// import pages
 import Landing from "../pages/Landing";
+import Anime from "~/pages/Anime";
 
+// routes
 export default function Routes() {
   return (
     <BrowserRouter>
       <Route path="/" exact component={Landing} />
+      <Route path="/anime" component={Anime} />
     </BrowserRouter>
   );
 }
